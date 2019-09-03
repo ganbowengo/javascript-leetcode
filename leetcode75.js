14667c84fe8791ca38529adcb81c34686b86620f
“ganbowengo” <1245960973@qq.com>
2019-09-03 11:20:16 +0800

    完成95 三色排列问题

---
+++ leetcode75.js
@@ -0,0 +1,54 @@
+// 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+// 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+// 注意:
+// 不能使用代码库中的排序函数来解决这道题。
+
+// 示例:
+
+// 输入: [2,0,2,1,1,0]
+// 输出: [0,0,1,1,2,2]
+// 进阶：
+
+// 一个直观的解决方案是使用计数排序的两趟扫描算法。
+// 首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+// 你能想出一个仅使用常数空间的一趟扫描算法吗？
+
+// 链接：https://leetcode-cn.com/problems/sort-colors
+// 优秀题解 -> https://leetcode-cn.com/problems/sort-colors/solution/yan-se-fen-lei-by-leetcode/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    let p1 = nums.length - 1
+    let current = 0,
+        p0 = 0
+    while (current <= p1) {
+        if (nums[current] === 0) {
+            swap(current, p0, nums)
+            p0++
+            current++
+            continue
+        }
+        if (nums[current] === 2) {
+            swap(current, p1, nums)
+            p1--
+            continue
+        }
+        current++
+    }
+    return nums
+};
+
+function swap(a, b, nums) {
+    let temp = nums[a]
+    nums[a] = nums[b]
+    nums[b] = temp
+}
+
+console.log(sortColors([2, 0, 2, 1, 1, 0]))
+console.log(sortColors([1, 2, 0]))
+console.log(sortColors([0, 1, 0]))
