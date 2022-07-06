9b3243f184ddd72ae526012d3ce7a9a5c0a93a3b
ganbowen <ganbowen@baiwang.com>
2022-07-06 23:32:43 +0800

    提交一道题

--- leetcode206.js
+++ leetcode206.js
@@ -51,32 +51,6 @@ var reverseList = function(head) {
     return prev
 };
 
-
-var reverseList = function(head) {
-    let prev = null
-    let curr = head
-    while(curr) {
-        const next = curr.next
-        curr.next = prev
-        prev = curr
-        curr = next
-    }
-    return prev
-};
-
-
-var reverseList = function(head) {
-    let prev = null
-    let curr = head
-    while(curr) {
-        const next = curr.next
-        curr.next = prev
-        prev = curr
-        curr = next
-    }
-    return prev
-};
-
 var reverseList = function(head) {
     if(head === null || head.next === null) return null
     const result = reverseList(head.next)
