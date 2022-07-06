a28e4fe3dd69b352b9f8654ffe6478762d2efa07
ganbowen <ganbowen@baiwang.com>
2022-07-06 23:32:09 +0800

    提交一道题

---
+++ leetcode206.js
@@ -0,0 +1,87 @@
+/*
+ * @Author       : ganbowen
+ * @Date         : 2022-07-06 22:41:34
+ * @LastEditors  : ganbowen
+ * @LastEditTime : 2022-07-06 23:31:57
+ * @Descripttion : 
+ */
+// 206. 反转链表
+// 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ 
+
+
+// 输入：head = [1,2,3,4,5]
+// 输出：[5,4,3,2,1]
+// 示例 2：
+
+
+// 输入：head = [1,2]
+// 输出：[2,1]
+// 示例 3：
+
+// 输入：head = []
+// 输出：[]
+
+function listNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+let a = new listNode(1)
+let b = a.next = new listNode(2)
+let e = b.next = new listNode(3)
+let f = e.next = new listNode(4)
+let g = f.next = new listNode(5)
+let h = f.next = new listNode(6)
+h.next = new listNode(7)
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null
+    let curr = head
+    while(curr) {
+        const next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next    
+    }
+    return prev
+};
+
+
+var reverseList = function(head) {
+    let prev = null
+    let curr = head
+    while(curr) {
+        const next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+};
+
+
+var reverseList = function(head) {
+    let prev = null
+    let curr = head
+    while(curr) {
+        const next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+};
+
+var reverseList = function(head) {
+    if(head === null || head.next === null) return null
+    const result = reverseList(head.next)
+    head.next.next = head
+    head.next = null
+    return result
+};
+console.log('reverseList', reverseList(a))
--- leetcode21.js
+++ leetcode21.js
@@ -1,3 +1,10 @@
+/*
+ * @Author       : ganbowen
+ * @Date         : 2019-10-27 15:05:01
+ * @LastEditors  : ganbowen
+ * @LastEditTime : 2022-07-06 22:38:30
+ * @Descripttion : 
+ */
 // 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
 
 // 示例：
@@ -55,4 +62,22 @@ var mergeTwoLists = function (l1, l2) {
     }
     
     return t.next ? t.next : null
+};
+
+
+var mergeTwoLists = function (l1, l2) {
+    if(l1 === null) {
+        return l2
+    }
+    if(l2 === null) {
+        return l1
+    }
+
+    if(l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+    }
 };
